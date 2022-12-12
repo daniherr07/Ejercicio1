fc83d38695fceca90c35c0ce9841fd96dfcf5fb7
Daniel <dherrera2195@gmail.com>
2022-12-11 20:07:28 -0600

    PrimerEjercicio

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,23 @@
 public class Ejercicio1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        System.out.println(sumar(1, 2, 3));
+
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+
+        System.out.println(miCoche.puertas);
     }
+
+    public static int sumar(int a, int b, int c){
+        return a + b +c; 
+    }
+}
+
+class Coche {
+    public int puertas = 2;
+
+    public void aumentarPuertas() {
+        this.puertas++;
+    }
+
 }
